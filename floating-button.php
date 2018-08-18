7dbe2c70ca9126d1de2bbbc8d740cfbcdf8f4186
Adnan Usman <adnantu@hotmail.com>
2018-08-17 21:18:37 -0700

    Version changed

--- floating-button.php
+++ floating-button.php
@@ -2,7 +2,7 @@
 /**
 * Plugin name: Floating Button WP
 * Description: Floating button to unveil anything you want, in my case it was a contact form.
-* Version: 1.1
+* Version: 1.2
 * Github: https://github.com/adnanusman
 * Github URI: https://github.com/adnanusman/floating-button-wp.git
 * License: GNU General Public License v2 or later
